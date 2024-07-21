a61b7035d2036dab7ea5905168c54c0770ab8806
2256907009 <2256907009@qq.com>
2024-07-22 00:28:38 +0800

    vue电商系统

---
+++ main.js
@@ -0,0 +1,30 @@
+import App from './App'
+
+import './network/axios_config.js'
+// 导入组件库
+import ElementUI from 'element-ui';
+// 导入组件相关样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 配置 Vue 插件
+Vue.use(ElementUI);
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
